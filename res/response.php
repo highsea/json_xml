8b01a9099471f190c408bc04267ac506a946dd0e
gao hai <wdssgaohai@qq.com>
2014-08-14 10:08:05 +0800

    array

--- res/response.php
+++ res/response.php
@@ -85,7 +85,8 @@ class Response{
 $data = array(
     'id'=>1,
     'name'=>'highsea',
-    'type'=>array(4, 5, 6)
+    'type'=>array(4, 5, 6),
+    'position'=>array('f2e', 88, 'ued'=>array('ui', 'engineer', 22),),
 );
 Response::xmlEncode(200, 'success', $data);
 ?>
